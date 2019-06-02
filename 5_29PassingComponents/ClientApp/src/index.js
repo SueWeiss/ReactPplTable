3e34a3c649e3e2829ab7f1a1f241e669d09b73b6
Sarah Weiss <suriw22@gmail.com>
2019-06-02 01:00:55 -0400

    Initial

--- 5_29PassingComponents/ClientApp/src/index.js
+++ 5_29PassingComponents/ClientApp/src/index.js
@@ -1,19 +1,70 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import ButtonCounter from './BottonCounter';
+import PersonSubmit from './PersonSubmit';
+import PersonTable from './Table';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+class App extends React.Component {
+    state = {
+            firstName: '',
+            lastName: '',
+            age: 0,        
+        peopleList: []
 
-ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
+    }
+    
+    clear = () => {
+        this.setState({ firstName: '', lastName: '', age: ' ' });
+    }
 
-registerServiceWorker();
+    onSubmitClick = () => {
+        const copyPersons =  [...this.state.peopleList];
+        const person = {
+            first: this.state.firstName,
+            last: this.state.lastName,
+            age: this.state.age
+        }
+        copyPersons.push(person);
+        this.setState({ peopleList: copyPersons })
+        this.clear();
+        
+            }
+
+    textChangeFirst = e => {
+        this.setState({ firstName : e.target.value });
+    }
+    textChangeLast = e => {
+        this.setState({ lastName : e.target.value });
+    }
+
+    textChangeAge = e => {
+        this.setState({ age : e.target.value });
+    }
+  
+
+    render() {
+        return (
+            <div className="container">
+                <PersonSubmit
+                    firstName={this.state.firstName}
+                    lastName={this.state.lastName}
+                    age={this.state.age}
+
+                    firstChange={this.textChangeFirst}
+                    lastChange={this.textChangeLast}
+                    ageChange={this.textChangeAge}
+                    submitClick={this.onSubmitClick} />
+
+
+                <div>
+                    <PersonTable
+                        peopleList={this.state.peopleList}
+                    />
+                </div>
+            </div>
+           
+        );
+    }
+}
+
+render(<App />, document.getElementById('root'));
